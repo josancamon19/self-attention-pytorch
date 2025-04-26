64021c0c64544314e90f639b2756a431569b09eb
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-04-26 13:29:16 -0700

    train utils + training function

--- decoder-only.py
+++ decoder-only.py
@@ -229,3 +229,63 @@ clip = 0.25
 dry_run = True
 epochs = 40
 
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Tensors, to detach them from their history."""
+
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def get_batch(source, i):
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i : i + seq_len]
+    target = source[i + 1 : i + 1 + seq_len].view(-1)
+    return data, target
+
+
+def train(model, epoch):
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0.0
+    start_time = time.time()
+    ntokens = len(corpus.dictionary)
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        model.zero_grad()
+        output = model(data)
+        output = output.view(-1, ntokens)
+        loss = criterion(output, targets)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        for p in model.parameters():
+            p.data.add_(p.grad, alpha=-lr)
+
+        total_loss += loss.item()
+
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss / log_interval
+            elapsed = time.time() - start_time
+            print(
+                "| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | "
+                "loss {:5.2f} | ppl {:8.2f}".format(
+                    epoch,
+                    batch,
+                    len(train_data) // bptt,
+                    lr,
+                    elapsed * 1000 / log_interval,
+                    cur_loss,
+                    math.exp(cur_loss),
+                )
+            )
+            total_loss = 0
+            start_time = time.time()
+        if dry_run:
+            break
+
