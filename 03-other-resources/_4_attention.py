64d49a10c0bdf06b3c3c0401f6044db0ee8be6d6
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-05-24 01:53:56 -0700

    attention viz + initial simple multi head attention example

---
+++ 03-other-resources/_4_attention.py
@@ -0,0 +1,58 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from _1_tokenization import  tokenize_input
+from _2_embedding import embed,config
+from _3_positional_encoding import add_positional_encoding
+
+
+def scaled_dot_product_attention(query, key, value):
+    dim_k = query.size(-1)
+    scores = torch.bmm(query, key.transpose(1, 2)) / np.sqrt(dim_k)
+    weights = F.softmax(scores, dim=-1)
+    return torch.bmm(weights, value)
+
+
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim):
+        super().__init__()
+        self.q = nn.Linear(embed_dim, head_dim)
+        self.k = nn.Linear(embed_dim, head_dim)
+        self.v = nn.Linear(embed_dim, head_dim)
+
+    def forward(self, hidden_state):
+        attn_outputs = scaled_dot_product_attention(
+            self.q(hidden_state), self.k(hidden_state), self.v(hidden_state)
+        )
+        return attn_outputs
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        embed_dim = config.embedding_dimensions
+        num_heads = config.num_attention_heads
+        head_dim = embed_dim // num_heads
+        self.heads = nn.ModuleList(
+            [AttentionHead(embed_dim, head_dim) for _ in range(num_heads)]
+        )
+        self.output_linear = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, hidden_state):
+        x = torch.cat([h(hidden_state) for h in self.heads], dim=-1)
+        x = self.output_linear(x)
+        return x
+
+
+if __name__ == "__main__":
+    text = "Hi, this is a test"
+    input_sequence = tokenize_input(text)
+    embedding_output = embed(input_sequence)
+    pos_enc_output = add_positional_encoding(embedding_output)
+
+    multihead_attn = MultiHeadAttention(config).to(config.device)
+    atn_output = multihead_attn(pos_enc_output)
+    print("Shape of output:", atn_output.size())
+    print("Number of heads:", len(MultiHeadAttention(config).heads))
+    multihead_attn.heads[:2]
