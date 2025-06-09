95e84c5c64913611520ddb32a33b1dd47d6b0250
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-06-09 12:02:09 -0700

    assignment 04 [part c]

--- 04-cs234/04/src/run.py
+++ 04-cs234/04/src/run.py
@@ -13,33 +13,36 @@ from torch.utils.tensorboard import SummaryWriter
 random.seed(0)
 
 argp = argparse.ArgumentParser()
-argp.add_argument('function', help="Choose pretrain, finetune, or evaluate")
-argp.add_argument('variant', help="Choose vanilla or rope")
-argp.add_argument('pretrain_corpus_path', default=None)
-argp.add_argument('--reading_params_path',default=None)
-argp.add_argument('--writing_params_path',default=None)
-argp.add_argument('--finetune_corpus_path', default=None)
-argp.add_argument('--eval_corpus_path', default=None)
-argp.add_argument('--outputs_path', default=None)
-argp.add_argument('--pretrain_lr', default=6e-3, type=float)
-argp.add_argument('--finetune_lr', default=6e-4, type=float)
-argp.add_argument('--tb_expt_name', help='debug string for tb log.',
-                  default='run')
+argp.add_argument("function", help="Choose pretrain, finetune, or evaluate")
+argp.add_argument("variant", help="Choose vanilla or rope")
+argp.add_argument("pretrain_corpus_path", default=None)
+argp.add_argument("--reading_params_path", default=None)
+argp.add_argument("--writing_params_path", default=None)
+argp.add_argument("--finetune_corpus_path", default=None)
+argp.add_argument("--eval_corpus_path", default=None)
+argp.add_argument("--outputs_path", default=None)
+argp.add_argument("--pretrain_lr", default=6e-3, type=float)
+argp.add_argument("--finetune_lr", default=6e-4, type=float)
+argp.add_argument("--tb_expt_name", help="debug string for tb log.", default="run")
 args = argp.parse_args()
 
-device = 'cpu'
+device = "cpu"
 if torch.cuda.is_available():
     device = torch.cuda.current_device()
-elif torch.backends.mps.is_available() and args.variant == 'vanilla':
-    device = 'mps'
+elif torch.backends.mps.is_available() and args.variant == "vanilla":
+    device = "mps"
 
 # TensorBoard training log
-writer = SummaryWriter(log_dir='expt/%s/%s_%s_pt_lr_%f_ft_lr_%f' % (
-    args.function,
-    args.tb_expt_name,
-    args.variant,
-    args.pretrain_lr,
-    args.finetune_lr))
+writer = SummaryWriter(
+    log_dir="expt/%s/%s_%s_pt_lr_%f_ft_lr_%f"
+    % (
+        args.function,
+        args.tb_expt_name,
+        args.variant,
+        args.pretrain_lr,
+        args.finetune_lr,
+    )
+)
 
 # Keep the block size 128
 # Why is the pretraining corpus always required (even if we're not pretraining?)
@@ -47,7 +50,7 @@ writer = SummaryWriter(log_dir='expt/%s/%s_%s_pt_lr_%f_ft_lr_%f' % (
 # (that is, the same mapping from character to integer, and we build the
 # vocab from the pretraining corpus.)
 block_size = 128
-text = open(args.pretrain_corpus_path, encoding='utf-8').read()
+text = open(args.pretrain_corpus_path, encoding="utf-8").read()
 pretrain_dataset = dataset.CharCorruptionDataset(text, block_size)
 
 # We don't suggest you change these hyperparameters, as they're known to work.
@@ -57,18 +60,19 @@ mconf = models.GPTConfig(
     pretrain_dataset.block_size,
     n_layer=4,
     n_head=8,
-    n_embd=256)
+    n_embd=256,
+)
 
 # define models.
 # note: models should moved to device defined on lines 30-34.
 
 model = None
-if args.variant == 'vanilla':
+if args.variant == "vanilla":
     # TODO: [part c] Make some model here
     ### YOUR CODE HERE ###
-    pass
+    model = models.GPT(mconf)
     ### END YOUR CODE ###
-elif args.variant == 'rope':
+elif args.variant == "rope":
     # TODO: [part g] Make some other model here
     # set mconf.rope parameter
     ### YOUR CODE HERE ###
@@ -77,10 +81,10 @@ elif args.variant == 'rope':
 else:
     raise ValueError("Unknown model variant")
 
-print('Model on device: ', next(model.parameters()).device)
+print("Model on device: ", next(model.parameters()).device)
 
 # Perform pretraining, finetuning, or evaluation
-if args.function == 'pretrain':
+if args.function == "pretrain":
     assert args.writing_params_path is not None
     # TODO [part f]:
     # - Given:
@@ -104,9 +108,10 @@ if args.function == 'pretrain':
     ### YOUR CODE HERE ###
     pass
     ### END YOUR CODE ###
-elif args.function == 'finetune':
+elif args.function == "finetune":
     assert args.writing_params_path is not None
     assert args.finetune_corpus_path is not None
+
     # TODO [part c] [part f]:
     # - Given:
     #     1. A finetuning corpus specified in args.finetune_corpus_path
@@ -141,29 +146,47 @@ elif args.function == 'finetune':
     #     number of epochs for each case.
 
     ### YOUR CODE HERE ###
-    pass
+    text = open(args.finetune_corpus_path, encoding="utf-8").read()
+    train_dataset = dataset.NameDataset(pretrain_dataset, text)
+    if args.reading_params_path is not None:
+        model.load_state_dict(torch.load(args.reading_params_path))
+
+    tconf = trainer.TrainerConfig(
+        max_epochs=10,
+        batch_size=256,
+        learning_rate=args.finetune_lr,
+        lr_decay=True,
+        warmup_tokens=512 * 20,
+        final_tokens=200 * len(pretrain_dataset) * block_size,
+        num_workers=0,
+        writer=writer,
+        ckpt_path=args.writing_params_path,
+    )
+    trainer = trainer.Trainer(model, train_dataset, None, tconf)
+    trainer.train()
     ### END YOUR CODE ###
-elif args.function == 'evaluate':
+elif args.function == "evaluate":
     assert args.outputs_path is not None
     assert args.reading_params_path is not None
     assert args.eval_corpus_path is not None
     model.load_state_dict(torch.load(args.reading_params_path))
     correct = 0
     total = 0
-    with open(args.outputs_path, 'w', encoding='utf-8') as fout:
+    with open(args.outputs_path, "w", encoding="utf-8") as fout:
         predictions = []
-        for line in tqdm(open(args.eval_corpus_path, encoding='utf-8')):
-            x = line.split('\t')[0]
-            x = x + '⁇'
-            x = torch.tensor([pretrain_dataset.stoi[s] for s in x],
-                             dtype=torch.long)[None,...].to(device)
+        for line in tqdm(open(args.eval_corpus_path, encoding="utf-8")):
+            x = line.split("\t")[0]
+            x = x + "⁇"
+            x = torch.tensor([pretrain_dataset.stoi[s] for s in x], dtype=torch.long)[
+                None, ...
+            ].to(device)
             pred = utils.sample(model, x, 32, sample=False)[0]
-            completion = ''.join([pretrain_dataset.itos[int(i)] for i in pred])
-            pred = completion.split('⁇')[1]
+            completion = "".join([pretrain_dataset.itos[int(i)] for i in pred])
+            pred = completion.split("⁇")[1]
             predictions.append(pred)
-            fout.write(pred + '\n')
+            fout.write(pred + "\n")
         total, correct = utils.evaluate_places(args.eval_corpus_path, predictions)
     if total > 0:
-        print(f'Correct: {correct} out of {total}: {correct/total*100}%')
+        print(f"Correct: {correct} out of {total}: {correct / total * 100}%")
     else:
-        print(f'Predictions written to {args.outputs_path}; no targets provided')
+        print(f"Predictions written to {args.outputs_path}; no targets provided")
