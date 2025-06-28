b56caf2f672d30010cc033f2d7dc6b64bbd17cf6
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-06-28 12:40:47 -0700

    masking, tokenizer, better init values

--- 05-cs336/01/cs336_basics/_transformer.py
+++ 05-cs336/01/cs336_basics/_transformer.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch
 from torch.nn.parameter import Parameter
 import torch.nn.functional as F
+from transformers import GPT2Tokenizer
 
-nn.Linear
 # We expect you to build these components from scratch. In particular, you may not
 # use any definitions from torch.nn, torch.nn.functional, or torch.optim except for the following:
 # • torch.nn.Parameter
@@ -16,57 +16,21 @@ class EmbeddingLayer(nn.Module):
     def __init__(self, vocab_size: int, embed_dim):
         super().__init__()
         self.embeddings = nn.Parameter(torch.empty((vocab_size, embed_dim)))
-        nn.init.uniform_(self.embeddings)  # -0.1, 0.1
-        # print(self.embeddings)
+        nn.init.normal_(self.embeddings, mean=0, std=0.02)
 
     def forward(self, token_ids: torch.tensor):
-        # How does this operation work
         return self.embeddings[token_ids]
-
-
-# emb = EmbeddingLayer(5, 2)
-# s1 = torch.arange(0, 5)
-# s2 = torch.ones(5, dtype=torch.int)
-# s3 = torch.tensor([1, 2, 3, 4, -1])
-
-# single = emb(s1)
-# batched = emb(torch.stack([s1, s2, s3]))
-# print("single.shape", single.shape)
-# print("batched.shape", batched.shape)
-# print("equal:", torch.equal(single, batched[0]))
 
 
 def get_positional_encodings(embed_dim, max_positions=3):
     positions = torch.arange(max_positions).unsqueeze(1)
-    # print(positions)
-    dim_indices = torch.arange(0, embed_dim, 2)
-    # print(dim_indices)
+    dim_indices = torch.arange(0, embed_dim, 2)  # TODO: not sure if this part is clear
     div_term = 10000 ** (dim_indices / embed_dim)
-    # print(div_term)
-    # print(positions / div_term)
 
     pe = torch.empty((max_positions, embed_dim))
     pe[:, 0::2] = torch.sin(positions / div_term)
     pe[:, 1::2] = torch.cos(positions / div_term)
-    # print(pe)
     return pe
-
-
-# get_positional_encodings(6, 5)
-
-embedding_dim = 128
-sequence_length = 5
-
-seq = torch.tensor([25, 32, 40, 41, 41])
-seq_batched = torch.stack([seq, seq, seq])
-
-# TODO: padding mask
-emb = EmbeddingLayer(1000, embedding_dim)
-pos_enc = get_positional_encodings(embedding_dim, 100)
-emb_pos_enc = emb(seq) + pos_enc[: len(seq)]
-# print(emb_pos_enc.shape) # 5, embedding_dim
-emb_pos_enc = emb(seq_batched) + pos_enc[: len(seq)]
-print("emb_pos_enc.shape:", emb_pos_enc.shape)  # 3, 5, embedding_dim
 
 
 class SelfAttention(nn.Module):
@@ -79,7 +43,11 @@ class SelfAttention(nn.Module):
         self.K = Parameter(torch.zeros(shape))
         self.V = Parameter(torch.zeros(shape))
 
-    def forward(self, x):
+        nn.init.normal_(self.Q, std=0.02)
+        nn.init.normal_(self.K, std=0.02)
+        nn.init.normal_(self.V, std=0.02)
+
+    def forward(self, x, padding_mask):
         # batch, seq_length, embedding_dim
         q = x @ self.Q
         k = x @ self.K
@@ -88,16 +56,19 @@ class SelfAttention(nn.Module):
         # TODO: Causal Mask
         # print("SelfAttention.forward q.shape:", q.shape)
         attention_scores = q @ k.transpose(1, 2)
+
+        mask = torch.tril(attention_scores) * (padding_mask.unsqueeze(1) if padding_mask is not None else 1)
+        attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+        # print(attention_scores)
+        # TODO: out of 7,7, which i,j refer to which tokens
+
         # print("SelfAttention.forward attention_scores:", attention_scores.shape)
         attention_weights = torch.softmax(attention_scores / math.sqrt(k.shape[-1]), dim=-1)
+        print(attention_weights)
         # print("SelfAttention.forward attention_weights:", attention_weights.shape)
         output = attention_weights @ v
         # print("SelfAttention.forward output:", output.shape)
         return output
-
-
-# sa = SelfAttention(128, 8)
-# sa(emb_pos_enc)
 
 
 class MultiHeadSelfAttention(nn.Module):
@@ -107,17 +78,13 @@ class MultiHeadSelfAttention(nn.Module):
         self.attention = nn.Sequential(*[SelfAttention(embedding_dim, num_heads) for _ in range(num_heads)])
         self.W_O = nn.Parameter(torch.zeros((embedding_dim, embedding_dim)))
 
-    def forward(self, x):
+    def forward(self, x, padding_mask):
         # print(x.shape)
-        output = [head(x) for head in self.attention]
+        output = [head(x, padding_mask) for head in self.attention]
         output = torch.cat(output, dim=-1)
         wo = output @ self.W_O
         print("MultiHeadSelfAttention.forward wo.shape:", wo.shape)
         return wo
-
-
-mhsa = MultiHeadSelfAttention(embedding_dim, 8)
-mhsa(emb_pos_enc)
 
 
 class TransformerBlock(nn.Module):
@@ -132,19 +99,18 @@ class TransformerBlock(nn.Module):
         self.mlp_out = nn.Parameter(torch.zeros((4 * embedding_dim, embedding_dim)))
         self.mlp_out_bias = nn.Parameter(torch.zeros((embedding_dim,)))
 
-    def forward(self, x):
+        nn.init.normal_(self.mlp_in, std=0.02)
+        nn.init.normal_(self.mlp_out, std=0.02)
+
+    def forward(self, x, padding_mask):
         # TODO: normalize
-        attention = self.mhsa(x) + x
+        attention = self.mhsa(x, padding_mask) + x
         # TODO: normalize
         proj_in = attention @ self.mlp_in + self.mlp_in_bias
         proj_in_activated = self.relu(proj_in)
         proj_out = proj_in_activated @ self.mlp_out + self.mlp_out_bias
         output = proj_out + attention
         return output
-
-
-# block = TransformerBlock(embedding_dim, 8)
-# block(emb_pos_enc)
 
 
 class Transformer(nn.Module):
@@ -164,17 +130,31 @@ class Transformer(nn.Module):
         # norm
         self.output = nn.Parameter(torch.zeros(embedding_dim, vocab_size))
 
-    def forward(self, input_ids):
+    def forward(self, input_ids, padding_mask):
         print(self.pe[: len(input_ids), :].shape)
         tokens = self.embeddings(input_ids) + self.pe[: input_ids.shape[-1], :]
         for block in self.blocks:
-            tokens = block(tokens)
+            tokens = block(tokens, padding_mask)
 
         output = tokens @ self.output
-        print("output.shape:", output.shape)
+        # print("output.shape:", output.shape)
         # return torch.softmax(output, dim=-1)
         return output  # output logits
 
 
-tf = Transformer(1000, 100, embedding_dim, 2, 8)
-prob_dist = tf(seq_batched)
+tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
+max_sequence_length = 100
+embedding_dim = 128
+num_layers = 1
+num_heads = 1
+
+tokenizer.pad_token = "[PAD]"
+tokenized = tokenizer(
+    ["Hi there, this is a test", "hey"],
+    return_tensors="pt",
+    padding=True,
+    truncation=True,
+)
+print(tokenized)
+tf = Transformer(tokenizer.vocab_size, max_sequence_length, embedding_dim, num_layers, num_heads)
+prob_dist = tf(tokenized["input_ids"], tokenized["attention_mask"])
