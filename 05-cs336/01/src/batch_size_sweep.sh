f1261166a050ef8ffc9440dcf4ac2f1492da518e
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-07-08 18:09:56 +0000

    minor batch size sweep script

--- 05-cs336/01/src/batch_size_sweep.sh
+++ 05-cs336/01/src/batch_size_sweep.sh
@@ -14,6 +14,11 @@
 # 1.44 1.51 < 5% diff
 # should consider running both 1 more or 2 more epochs
 
-python src/train_transformer.py --batch-size 64 --epochs 1 --lr-max 3e-4 -c ".models/gpt2-epoch-1-lr-0.0003-batch-64.pt" --wandb-id "distinctive-voice-27"
+# python src/train_transformer.py --batch-size 64 --epochs 1 --lr-max 3e-4 -c ".models/gpt2-epoch-1-lr-0.0003-batch-64.pt" --wandb-id "distinctive-voice-27"
 
-python src/train_transformer.py --batch-size 128 --epochs 1 --lr-max 3e-4 -c ".models/gpt2-epoch-1-lr-0.0003-batch-128.pt" --wandb-id "expert-yogurt-25"
+# python src/train_transformer.py --batch-size 128 --epochs 1 --lr-max 3e-4 -c ".models/gpt2-epoch-1-lr-0.0003-batch-128.pt" --wandb-id "expert-yogurt-25"
+
+# trying each one of the good batch sizes a bit longer, 2 epochs.
+python src/train_transformer.py --batch-size 64 --epochs 2 --lr-max 3e-4
+# python src/train_transformer.py --batch-size 96 --epochs 2 --lr-max 3e-4
+# python src/train_transformer.py --batch-size 128 --epochs 2 --lr-max 3e-4
