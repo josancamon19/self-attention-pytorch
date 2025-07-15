8c763c45cc97174d22216ecdd897f1d95df0f885
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-07-15 12:13:32 -0700

    assignment 02 1.1.4

---
+++ 05-cs336/02/cs336_systems/main.py
@@ -0,0 +1,96 @@
+import timeit
+from cs336_basics.model import BasicsTransformerLM
+import torch
+from enum import Enum
+
+# Notes
+
+# - For all models, we’ll use a vocabulary size of 10,000 and a batch size of 4, with varying context lengths.
+# - automate constructing tables for your writeup in code pandas.DataFrame.to_latex()
+# - model sizes table
+# - we'll be testing precision changes, swapping layers, have your script enable this variations through cli ✅
+# - suggests using `sbatch or submitit on Slurm`, why not ray? for quick iteration
+# 1. timing fwd backward passes
+
+# When you call a CUDA kernel,
+# such as when you invoke torch.matmul, the function call returns control to your code without waiting for
+# the matrix multiplication to finish. In this way, the CPU can continue running while the GPU computes the
+# matrix multiplication. (start_time - time.time() doesn't work at all)
+# torch.cuda.synchronize() to wait for all GPU kernels to complete
+
+vocab_size = 10000
+batch_size = 4
+
+model_configs = [
+    {"name": "small", "d_model": 768, "d_ff": 3072, "num_layers": 12, "num_heads": 12},
+    {"name": "medium", "d_model": 1024, "d_ff": 4096, "num_layers": 24, "num_heads": 16},
+    {"name": "large", "d_model": 1280, "d_ff": 5120, "num_layers": 36, "num_heads": 20},
+    {"name": "xl", "d_model": 1600, "d_ff": 6400, "num_layers": 48, "num_heads": 25},
+    {"name": "2.7B", "d_model": 2560, "d_ff": 10240, "num_layers": 32, "num_heads": 32},
+]
+
+
+def get_model(seq_length, d_model, num_layers, num_heads, dff, rope_theta=10000):
+    return BasicsTransformerLM(
+        vocab_size,
+        seq_length,
+        d_model,
+        num_layers,
+        num_heads,
+        dff,
+        rope_theta,
+    )
+
+
+def get_random_batch(seq_length: int, padding_size: int = None):
+    input_ids = torch.randint(0, vocab_size, (batch_size, seq_length))
+    if not padding_size:
+        return input_ids
+
+    # assert padding_size >= seq_length
+    # padding_mask = torch.zeros_like(input_ids)
+
+
+def run_warmup(model: torch.nn.Module, n_steps: int = 100):
+    for _ in range(n_steps):
+        model(get_random_batch(512))
+
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+
+
+class MeasurementType(Enum):
+    FORWARD = "forward"
+    BACKWARD = "backward"  # backward includes both
+
+
+def measure(
+    model: torch.nn.Module,
+    n_steps: int,
+    type: MeasurementType = MeasurementType.BACKWARD,
+) -> float:
+    start = timeit.default_timer()
+    for _ in range(n_steps):
+        output = model(get_random_batch(), None)
+        if type == MeasurementType.BACKWARD:
+            output.sum().backward()
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+    total = timeit.default_timer() - start
+    print("[measure] n_steps:", n_steps, type)
+    return total
+
+
+if __name__ == "__main__":
+    for config in model_configs:
+        model = get_model(
+            seq_length=512,
+            d_model=config["d_model"],
+            num_layers=config["num_layers"],
+            num_heads=config["num_heads"],
+            dff=config["d_ff"],
+            rope_theta=config.get("rope_theta", 10000),
+        )
+        run_warmup(model)
+        total_time = measure(model, n_steps=5)
+        print(f"Config: {config}, Time: {total_time:.4f} seconds")
