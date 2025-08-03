corrected forward backward for causal, was skipping tiles incorrectly

05-cs336/02/src/flash_triton/benchmark.py
@@ -58,12 +58,12 @@ def verify_correctness(dtype=torch.float32):
     test_configs = [
         # (n_heads, seq_length, d_head, is_causal, desc)
         # (4, 128, 64, False, "small non-causal"),
-        (4, 128, 64, True, "small causal"),
         # (8, 512, 64, False, "medium non-causal"),
-        (8, 512, 64, True, "medium causal"),
         # (12, 1024, 64, False, "large non-causal"),
+        (4, 128, 64, True, "small causal"),
+        (8, 512, 64, True, "medium causal"),
         (12, 1024, 64, True, "large causal"),
-        (16, 16384, 64, False, "huge causal"),
+        (16, 16384, 64, True, "huge causal"),
     ]
     for n_heads, seq_length, d_head, is_causal, desc in test_configs:
         q, k, v = get_q_k_v(n_heads, seq_length, d_head, dtype)
@@ -123,8 +123,8 @@ def verify_correctness(dtype=torch.float32):
 
 
 if __name__ == "__main__":
-    verify_correctness(dtype=torch.bfloat16)
-    # flash_benchmarking()
+    # verify_correctness(dtype=torch.bfloat16)
+    flash_benchmarking()
     # results
     # - backward implementation atomic, from 28 to 27
     # - backward 2 passes, from 27 to 20
@@ -134,6 +134,11 @@ if __name__ == "__main__":
     # - exp2, 9.55
     # - Separate causal phases forward: 1.72ms → 1.45ms, overall 9.6ms → 9.38ms
     # - Separate causal phases backward: 9.38ms → 4.82ms 50% better holy shit
+    # - - this backward, works only for the target leaderboard setup, is off for other sizes
+    # - autotuned again, got 4.139172560082744, 15%?
+    # - - definitely something regarding the grid size works perfectly for this matrix size. Is it something regarding dimensions? head_dim/seq_length?
+    # - - nvm, benchmark was wrong, I was not setting causal to True!!
+
     # TODO:
     # - Use Persistent Matmul?
 

05-cs336/02/src/flash_triton/flash.py
@@ -39,10 +39,11 @@ class FlashAttention(torch.autograd.Function):
 
         # def reshape(m): return m.reshape((batch_heads, seq_length, d))  # noqa: E731
 
-        q_tile_size, k_tile_size, num_warps, num_stages = 64, 64, 4, 3
-        # q_tile_size, k_tile_size, num_warps, num_stages = 128, 256, 8, 3 # autotune value performs worst (?)
-        grid = (triton.cdiv(seq_length, q_tile_size), batch_heads)
-        # grid = lambda meta: (triton.cdiv(seq_length, meta["BLOCK_SIZE_Q"]), batch_heads)  # noqa: E731
+        # q_tile_size, k_tile_size, num_warps, num_stages = 64, 64, 4, 3
+        # autotune value performs worst (?)
+        # q_tile_size, k_tile_size, num_warps, num_stages = 128, 64, 4, 4
+        # grid = (triton.cdiv(seq_length, q_tile_size), batch_heads)
+        grid = lambda meta: (triton.cdiv(seq_length, meta["BLOCK_SIZE_Q"]), batch_heads)  # noqa: E731
 
         # flash_forward[grid](
         flash_forward_tma[grid](
@@ -57,10 +58,10 @@ class FlashAttention(torch.autograd.Function):
             seq_length=seq_length,
             head_dim=d,
             is_causal=is_causal,
-            BLOCK_SIZE_Q=q_tile_size,
-            BLOCK_SIZE_K=k_tile_size,
-            num_warps=num_warps,
-            num_stages=num_stages,
+            # BLOCK_SIZE_Q=q_tile_size,
+            # BLOCK_SIZE_K=k_tile_size,
+            # num_warps=num_warps,
+            # num_stages=num_stages,
             # num_ctas=num_ctas, # not include, worst results.
             # WARP_SPECIALIZE=False,  # TMA + warp specialization causes race conditions
         )
@@ -168,12 +169,9 @@ class FlashAttention(torch.autograd.Function):
         # def reshape(m): return m.reshape((batch_heads, seq_length, d))
         def reshape(m): return m
 
-        # PASS 1: Compute grad_q (parallelize over Q blocks)
-        # LOCK_SIZE_Q: 128, BLOCK_SIZE_K: 64, num_warps: 8, num_ctas: 1, num_stages: 5,
-        BLOCK_SIZE_Q, BLOCK_SIZE_K, num_warps, num_stages = 256, 64, 8, 5
-        # BLOCK_SIZE_Q, BLOCK_SIZE_K, num_warps, num_stages = 64, 64, 4, 3 # thought maybe defaults would be better
-        grid = (triton.cdiv(seq_length, BLOCK_SIZE_Q), batch_heads)
-        # grid = lambda meta: (triton.cdiv(seq_length, meta['BLOCK_SIZE_Q']), batch_heads)
+        # BLOCK_SIZE_Q, BLOCK_SIZE_K, num_warps, num_stages = 256, 64, 8, 3  # 3 or 5
+        # grid = (triton.cdiv(seq_length, BLOCK_SIZE_Q), batch_heads)
+        grid = lambda meta: (triton.cdiv(seq_length, meta['BLOCK_SIZE_Q']), batch_heads)
         flash_backward_pass1_grad_q[grid](
             reshape(grad_out),
             D.reshape(batch_heads, seq_length),
@@ -186,16 +184,17 @@ class FlashAttention(torch.autograd.Function):
             seq_length,
             d,
             ctx.is_causal,
-            BLOCK_SIZE_Q,
-            BLOCK_SIZE_K,
-            num_warps=num_warps,
-            num_stages=num_stages,
+            # BLOCK_SIZE_Q,
+            # BLOCK_SIZE_K,
+            # num_warps=num_warps,
+            # num_stages=num_stages,
         )
 
         # PASS 2: Compute grad_k and grad_v (parallelize over K blocks)
-        BLOCK_SIZE_Q, BLOCK_SIZE_K, num_warps, num_stages = 64, 64, 4, 3
-        grid = (triton.cdiv(seq_length, BLOCK_SIZE_K), batch_heads)
-        # grid = lambda meta: (triton.cdiv(seq_length, meta['BLOCK_SIZE_K']), batch_heads)
+        # BLOCK_SIZE_Q, BLOCK_SIZE_K, num_warps, num_stages = 64, 64, 4, 3
+        # grid = (triton.cdiv(seq_length, BLOCK_SIZE_K), batch_heads)
+        def grid(meta): return (triton.cdiv(
+            seq_length, meta['BLOCK_SIZE_K']), batch_heads)
         flash_backward_pass2_grad_kv[grid](
             reshape(grad_out),
             D.reshape(batch_heads, seq_length),
@@ -209,10 +208,10 @@ class FlashAttention(torch.autograd.Function):
             seq_length,
             d,
             ctx.is_causal,
-            BLOCK_SIZE_Q,
-            BLOCK_SIZE_K,
-            num_warps=num_warps,
-            num_stages=num_stages,
+            # BLOCK_SIZE_Q,
+            # BLOCK_SIZE_K,
+            # num_warps=num_warps,
+            # num_stages=num_stages,
         )
 
         # Reshape gradients to match input shapes